41565b29150351ce2e07775683f0cf800b24663d
Hunor Donkó <donko.hunor.attila@gmail.com>
2023-09-26 08:50:36 +0200

    Bekeres metódus létehozása és a lista metódusok számára is elérhetővé tétele

--- Program.cs
+++ Program.cs
@@ -8,11 +8,41 @@ namespace DonkoHunor_BejegyzesProjekt
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static List<Bejegyzes> list = new List<Bejegyzes>();
+        
+        static void Bekeres()
         {
-            List<Bejegyzes> list = new List<Bejegyzes>();
+            Console.Write("Adjon meg egy darabszámot! : ");
+            string db = Console.ReadLine();
+            Console.WriteLine();
+            while (!int.TryParse(db, out int result))
+            {
+                Console.WriteLine("Nem jó számot adott meg!");
+                Console.Write("Adjon meg egy darabszámot! : ");
+                db = Console.ReadLine();
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < int.Parse(db); i++) 
+            {
+                Console.Write("Adja meg a tartalmat: ");
+                string tartalom = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Adja meg a szerző nevét: ");
+                string szerzo = Console.ReadLine();
+                Console.WriteLine();
+                list.Add(new Bejegyzes(szerzo,tartalom));
+            }
+        }
+
+        static void Main(string[] args)
+        {            
             list.Add(new Bejegyzes("Balázs Dániel","Az én túróm is szaftos."));
             list.Add(new Bejegyzes("Szun-ce","Dying is gay."));
+            
+            Bekeres();
+            
             Console.ReadKey();
         }
     }
